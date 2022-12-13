4d43e1909aa60492e68a1fa8d9eb7c63e7e680b8
rezwanatech <rezwanatech@gmail.com>
2022-12-13 10:58:22 -0500

    completed portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+const myInfo = {
+  myName: "Rezwana",
+  myPronouns: "she/her",
+  date: "12/05/2022",
+  myHobbies: ["Journal", "Painting"],
+  isEnrolled: true
+}
+
+console.log("Hello my name is " + myInfo.myName + " (" + myInfo.myPronouns + ") " + "and my SEI course is going to start on " + myInfo.date + ".");
